59030ff7266f16419cde9c2da3dc7c435b223848
jealob <jealob@gmail.com>
2018-05-30 17:56:17 -0500

    adds arrayLetter to word.js

--- word.js
+++ word.js
@@ -1,5 +1,14 @@
 let Letter = require("./letter.js");
+
 let Word = function (word) {
     // console.log("runs");
     this.letterArray = [];
- 
+    // Push all letter string to array
+    word.split("").forEach(function (element) {
+        this.letterArray.push(new Letter(element));
+    });
+   
+
+}
+
+module.exports = Word;
